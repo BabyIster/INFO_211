ca8e1d03b17374209aa873bac47d6a86f8337263
DrFlofloFixe <drflo44@gmail.com>
2021-02-17 14:00:28 +0100

    ajout session bean

---
+++ CabinetRecrutement_EJB/ejbModule/eu/telecom_bretagne/cabinet_recrutement/service/ServicesBean.java
@@ -0,0 +1,20 @@
+package eu.telecom_bretagne.cabinet_recrutement.service;
+
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+
+/**
+ * Session Bean implementation class ServicesBean
+ */
+@Stateless
+@LocalBean
+public class ServicesBean {
+
+    /**
+     * Default constructor. 
+     */
+    public ServicesBean() {
+        // TODO Auto-generated constructor stub
+    }
+
+}
